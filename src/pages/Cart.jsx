54d47dabd15270e8e716bf6b62acd1591b24c190
izanloo <razieh.izanloo755@gmail.com>
2022-11-02 04:52:20 -0700

    feat:add easy edit for chache count product in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,50 +1,64 @@
-import { Fragment, useState,useEffect } from "react";
+import { Fragment, useState, useEffect } from "react";
 import WithUser from "../layouts/WithUser";
 
 function Cart() {
-    let getLocalstorage = JSON.parse(localStorage.getItem('cart'))
+    let dataLocalStorage = JSON.parse(localStorage.getItem('cart'))
     let [sum, setSum] = useState()
 
     function sumPrices() {
         let arr = [];
         let sums
-        if (getLocalstorage == null) {
-          console.log("null")
+        if (dataLocalStorage == null) {
+            console.log("null")
         } else {
-          const data = localStorage.getItem('cart');
-          const initialData = data !== null ? JSON.parse(data) : null;
-          initialData.map(item => {
-            arr = [...arr, parseInt(item.count) * parseInt(item.price)]
-            sums = arr.reduce((x, y) => x + y);
-          })
+            const data = localStorage.getItem('cart');
+            const initialData = data !== null ? JSON.parse(data) : null;
+            initialData.map(item => {
+                arr = [...arr, parseInt(item.count) * parseInt(item.price)]
+                sums = arr.reduce((x, y) => x + y);
+            })
         }
         setSum(sums)
-      }
-      useEffect(() => { sumPrices() }, [])
-      console.log(sum)
+    }
+    useEffect(() => { sumPrices() }, [])
+
+    //------ edit count product
+    const handleEdit = (item) => {
+        document.getElementById("editCount").setAttribute("contenteditable", "true");
+        const box = document.getElementById("editCount");
+        const findItem = dataLocalStorage.findIndex(i => i.id === item.id)
+        let aa = box.textContent;
+        if (findItem >= 0) {
+            dataLocalStorage.splice(findItem, 1)
+            localStorage.setItem('cart', JSON.stringify(dataLocalStorage));
+            const newLocal = JSON.parse(localStorage.getItem("cart"));
+            let newRow = { "name": item.name, "price": item.price, "count": "2", "id": item.id };
+            localStorage.setItem('cart', JSON.stringify([...newLocal, newRow]));
+            sumPrices()
+        }
+    }
     return (
         <div className="py-40 flex justify-center">
-       {getLocalstorage == null ? "محصولی در سبدخرید شما وجود ندارد" :
-        <>
-        <table className="border text-center">
-            <th className="border px-2">نام محصول</th>
-            <th className="border px-2">تعداد</th>
-            <th className="border px-2">قیمت واحد</th>
-            <th className="border px-2">مجموع قیمت</th>
-        {Object.values(getLocalstorage).map((item, id) => (
-                    <tr key={id}>
-                        <td className="border px-2">{item.name}</td>
-                        <td className="border px-2">{item.count}</td>
-                        <td className="border px-2">{item.price}</td>
-                        <td className="border px-2">{item.price*item.count}</td>
-                    </tr>
-        ))}
-        </table>
-        <h2>جمع: {sum == null ? "0" : <span>{sum} </span>} تومان  </h2>
+            {dataLocalStorage == null ? "محصولی در سبدخرید شما وجود ندارد" :
+                <>
+                    <table className="border text-center">
+                        <th className="border px-2">نام محصول</th>
+                        <th className="border px-2">تعداد</th>
+                        <th className="border px-2">قیمت واحد</th>
+                        {Object.values(dataLocalStorage).map((item, id) => (
+                            <tr key={id}>
+                                <td className="border px-2">{item.name}</td>
+                                <td className="border px-2" id="editCount" onClick={() => handleEdit(item)}>{item.count}</td>
+                                <td className="border px-2">{item.price}</td>
+                            </tr>
+                        ))}
+                    </table>
+                    <h2>جمع: {sum == null ? "0" : <span>{sum} </span>} تومان  </h2>
 
-    </>
-       }
-       </div>
+                </>
+            }
+        </div>
     )
 }
+// npm i react-easy-edit
 export default WithUser(Cart)
